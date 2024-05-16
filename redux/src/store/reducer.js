f576ebf2ff9a2bc96c77e42931ced1dbea4b5c0c
Jhonnier Hernandez <jhonnier.hernandez@correounivalle.edu.co>
2024-05-16 18:11:56 -0500

    primer commit

---
+++ redux/src/store/reducer.js
@@ -0,0 +1,14 @@
+const initalState = 0
+
+const counterReducer = (state = initalState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+export default counterReducer;
